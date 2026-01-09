c0515d83e4e64df124670c9e8dbec1adba7c30d9
nicolasdev97 <nicolas.po.im@gmail.com>
2026-01-09 16:20:13 -0500

    Upload the phonebook backend configuration.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
 
 app.use(morgan("tiny"));
+
+app.use(cors());
 
 let persons = [
   {
@@ -116,7 +120,8 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
