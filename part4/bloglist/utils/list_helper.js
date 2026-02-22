293f960bacc902b8fd8dc04b2dac11fcf6a3de62
nicolasdev97 <nicolas.po.im@gmail.com>
2026-02-22 18:21:19 -0500

    Exercise 4.5.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const favorite = blogs.reduce((prev, current) => {
+    return current.likes > prev.likes ? current : prev;
+  });
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
