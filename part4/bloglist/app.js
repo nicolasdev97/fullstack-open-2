6ef5c455f2064d9af968296a6b48c4b7b89d8a1f
nicolasdev97 <nicolas.po.im@gmail.com>
2026-02-22 12:59:34 -0500

    Exercise 4.2.

---
+++ part4/bloglist/app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+// Configure express app and middlewares
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Import routers
+
+const blogsRouter = require("./controllers/blogs");
+
+// Use middlewares
+
+app.use(cors());
+app.use(express.json());
+
+// Use routers
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// Define the schema for the database entries
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
