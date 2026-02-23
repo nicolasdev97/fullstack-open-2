445355d33ae4723ae3f4072657d1179d1e3df977
nicolasdev97 <nicolas.po.im@gmail.com>
2026-02-23 11:12:02 -0500

    Exercise 4.13.

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -115,6 +115,20 @@ test("blog without url is not added", async () => {
   await api.post("/api/blogs").send(newBlog).expect(400);
 });
 
+test("a blog can be deleted", async () => {
+  const blogsAtStart = await Blog.find({});
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await Blog.find({});
+
+  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1);
+
+  const titles = blogsAtEnd.map((b) => b.title);
+  assert(!titles.includes(blogToDelete.title));
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
