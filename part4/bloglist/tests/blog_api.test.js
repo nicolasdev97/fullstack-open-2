5e35e3d4813f45df19baebc93c024bb47bb6a37c
nicolasdev97 <nicolas.po.im@gmail.com>
2026-02-23 10:49:54 -0500

    Exercise 4.9.

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -46,6 +46,15 @@ test("all blogs are returned", async () => {
   assert.strictEqual(response.body.length, initialBlogs.length);
 });
 
+test("blog posts have id property defined", async () => {
+  const response = await api.get("/api/blogs");
+
+  const blog = response.body[0];
+
+  assert(blog.id !== undefined);
+  assert.strictEqual(blog._id, undefined);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
