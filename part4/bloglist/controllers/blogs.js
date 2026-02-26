a7899b572212052e049880560acadc4fb8485687
nicolasdev97 <nicolas.po.im@gmail.com>
2026-02-26 12:25:39 -0500

    Exercise 4.17.

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,32 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 // Define routes and their logic
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  try {
-    const blog = new Blog(request.body);
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch (error) {
-    response.status(400).json({ error: error.message });
-  }
+  const body = request.body;
+
+  const user = await User.findOne({});
+
+  const blog = new Blog({
+    ...body,
+    likes: body.likes || 0,
+    user: user._id,
+  });
+
+  const savedBlog = await blog.save();
+
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
