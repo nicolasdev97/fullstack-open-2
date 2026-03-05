7fe7c4acf2210c9ee4b8d7de1ca1bb044cf93180
nicolasdev97 <nicolas.po.im@gmail.com>
2026-03-05 15:58:50 -0500

    Exercise 7.7.

---
+++ part7/country-hook/src/hooks/useCountry.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    if (!name) return;
+
+    const fetchCountry = async () => {
+      try {
+        const response = await axios.get(
+          `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`,
+        );
+
+        setCountry({
+          found: true,
+          data: response.data,
+        });
+      } catch (error) {
+        setCountry({
+          found: false,
+        });
+      }
+    };
+
+    fetchCountry();
+  }, [name]);
+
+  return country;
+};
