8eea4702e694acdad605c4e59f0a4890ab1e7c20
nicolasdev97 <nicolas.po.im@gmail.com>
2026-04-20 17:27:03 -0500

    Exercise 8.11.

--- part8/library-frontend/src/components/Authors.jsx
+++ part8/library-frontend/src/components/Authors.jsx
@@ -1,14 +1,36 @@
-import { useQuery } from "@apollo/client/react";
+import { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client/react";
 import { ALL_AUTHORS } from "../graphql/queries";
+import { EDIT_AUTHOR } from "../graphql/mutations";
 
 const Authors = () => {
   const { loading, data } = useQuery(ALL_AUTHORS);
+
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
 
   if (loading) {
     return <div>loading...</div>;
   }
 
   const authors = data.allAuthors;
+
+  const submit = (event) => {
+    event.preventDefault();
+
+    editAuthor({
+      variables: {
+        name,
+        setBornTo: Number(born),
+      },
+    });
+
+    setBorn("");
+  };
 
   return (
     <div>
@@ -29,6 +51,29 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
+      <h3>set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          author
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            <option value="">select author</option>
+            {authors.map((a) => (
+              <option key={a.name} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
