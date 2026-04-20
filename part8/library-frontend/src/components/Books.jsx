6b68b72d5f9ebebad254c70f7face536a0900244
nicolasdev97 <nicolas.po.im@gmail.com>
2026-04-20 13:30:41 -0500

    Exercise 8.9.

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,14 @@
-const Books = (props) => {
-  if (!props.show) {
-    return null
+import { useQuery } from "@apollo/client/react";
+import { ALL_BOOKS } from "../graphql/queries";
+
+const Books = () => {
+  const { loading, data } = useQuery(ALL_BOOKS);
+
+  if (loading) {
+    return <div>loading...</div>;
   }
 
-  const books = []
+  const books = data.allBooks;
 
   return (
     <div>
@@ -12,21 +17,21 @@ const Books = (props) => {
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {books.map((b) => (
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author}</td>
+              <td>{b.published}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
