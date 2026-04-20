4599b298acc467618ad4c6d2d0049aec6e3af818
nicolasdev97 <nicolas.po.im@gmail.com>
2026-04-20 11:25:21 -0500

    Exercise 8.7.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,6 +102,11 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book!
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 
   type Book {
@@ -170,6 +175,16 @@ const resolvers = {
       }
 
       return newBook;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+
+      if (!author) {
+        return null;
+      }
+
+      author.born = args.setBornTo;
+      return author;
     },
   },
 };
