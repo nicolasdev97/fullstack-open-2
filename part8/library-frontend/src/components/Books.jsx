cf98a22c940dbf620c4b3c00c32ec718de6df220
nicolasdev97 <nicolas.po.im@gmail.com>
2026-04-24 08:22:40 -0500

    Exercise 8.19.

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,8 +1,10 @@
 import { useQuery } from "@apollo/client/react";
 import { ALL_BOOKS } from "../graphql/queries";
+import { useState } from "react";
 
 const Books = ({ show }) => {
   const { loading, data } = useQuery(ALL_BOOKS);
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
   if (!show) {
     return null;
@@ -13,6 +15,12 @@ const Books = ({ show }) => {
   }
 
   const books = data.allBooks;
+
+  const genres = [...new Set(books.flatMap((book) => book.genres))];
+
+  const filteredBooks = selectedGenre
+    ? books.filter((book) => book.genres.includes(selectedGenre))
+    : books;
 
   return (
     <div>
@@ -25,7 +33,7 @@ const Books = ({ show }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((b) => (
+          {filteredBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -34,6 +42,16 @@ const Books = ({ show }) => {
           ))}
         </tbody>
       </table>
+
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+
+        <button onClick={() => setSelectedGenre(null)}>all genres</button>
+      </div>
     </div>
   );
 };
