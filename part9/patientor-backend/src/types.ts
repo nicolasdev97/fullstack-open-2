0e364c5d9dd838f237a0c9fb1659961ab8b979de
nicolasdev97 <nicolas.po.im@gmail.com>
2026-04-29 17:02:54 -0500

    Exercise 9.22.

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -26,3 +23,45 @@ export interface Diagnosis {
   name: string;
   latin?: string;
 }
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: string[];
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export enum HealthCheckRating {
+  Healthy = 0,
+  LowRisk = 1,
+  HighRisk = 2,
+  CriticalRisk = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
