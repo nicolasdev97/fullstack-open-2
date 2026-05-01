b9e0e2b9ae86280a98f7b2b1c22ad0f9655e233c
nicolasdev97 <nicolas.po.im@gmail.com>
2026-05-01 15:41:29 -0500

    Fixing the font mistake.

--- part10/rate-repository-app/constants/theme.ts
+++ part10/rate-repository-app/constants/theme.ts
@@ -1,40 +1,35 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
 import { Platform } from "react-native";
 
 const tintColorLight = "#0a7ea4";
 const tintColorDark = "#fff";
 
-export const Colors = {
-  light: {
-    text: "#11181C",
-    background: "#fff",
-    tint: tintColorLight,
-    icon: "#687076",
-    tabIconDefault: "#687076",
-    tabIconSelected: tintColorLight,
+const theme = {
+  colors: {
+    light: {
+      text: "#11181C",
+      background: "#fff",
+      tint: tintColorLight,
+      icon: "#687076",
+      tabIconDefault: "#687076",
+      tabIconSelected: tintColorLight,
+    },
+    dark: {
+      text: "#ECEDEE",
+      background: "#151718",
+      tint: tintColorDark,
+      icon: "#9BA1A6",
+      tabIconDefault: "#9BA1A6",
+      tabIconSelected: tintColorDark,
+    },
   },
-  dark: {
-    text: "#ECEDEE",
-    background: "#151718",
-    tint: tintColorDark,
-    icon: "#9BA1A6",
-    tabIconDefault: "#9BA1A6",
-    tabIconSelected: tintColorDark,
+
+  fonts: {
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System",
+    }),
   },
 };
 
-export const Fonts = Platform.select({
-  android: {
-    sans: "Roboto",
-  },
-  ios: {
-    sans: "Arial",
-  },
-  default: {
-    sans: "System",
-  },
-});
+export default theme;
